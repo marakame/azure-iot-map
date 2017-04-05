0fb16ad6d025635cb0a7e4ee1f8beefef037c454
Daniel Sánchez Pillot Gutiérrez <marakame666@gmail.com>
2017-04-05 01:01:45 -0500

    Format and comment code

--- app.js
+++ app.js
@@ -4,30 +4,36 @@ var express = require('express'),
 var jquery = require('jquery');
 var deviceReaderModule = require('./read-device-to-cloud-messages');
 
+// Create express app
 var app = express();
 var server = require('http').Server(app);
 
+// We will use socket.io for sending and receiving realtime data through websockets
 var io = require('socket.io')(server);
 
+// If running in Azure process.env.port will get the port assigned to the app
+// If running locally the app will use a fixed port
 var port = process.env.port || 3000
-//setup our app to use handlebars.js for templating
+
+// Setup the app to use handlebars.js for templating
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+	extended: true
 }));
 
-// Routes
+// Setup routes
 app.get('/', function(req, res) {
     res.render('index');
 });
 
 app.use(express.static(__dirname + '/static'));
 
-//have our app listen on azure port local port 3000
+// Have the app listen on the port defined previously
 server.listen(port);
 
+// Start the websocket communications
 io.on('connection', function (socket) {
 	var deviceReader = new deviceReaderModule(socket);
 
--- read-device-to-cloud-messages.js
+++ read-device-to-cloud-messages.js
@@ -2,15 +2,20 @@
 
 module.exports = function(socket) {
 
+    // Create the websocket and event hub client
     var socket = socket;
     var EventHubClient = require('azure-event-hubs').Client;
 
+    // Event hub client connection string
     var connectionString = 'HostName={AppHostName};SharedAccessKeyName={YourSharedAccessKeyName};SharedAccessKey={YourSharedAccessKey}';
 
+    // Function to print errors to the console
     var printError = function (err) {
       console.log(err.message);
     };
 
+    // Function to print messages to the console and emit the corresponding
+    // message to the map via websockets
     var printMessage = function (message) {
       console.log('Message received: ');
       console.log(JSON.stringify(message.body));
@@ -20,12 +25,17 @@ module.exports = function(socket) {
       socket.emit('device-update', message.body);
     };
 
+    // Create the event hub client
     var client = EventHubClient.fromConnectionString(connectionString);
 
-    // Define listening function
+    // The startListening function will open an event hub client and start listening
+    // for messages, when a message is received, the message will be emitted via
+    // websockets to the web map
     this.startListening = function() {
         client.open()
+            // Bind the client to the partitionIds of the Azure IoT Hub
             .then(client.getPartitionIds.bind(client))
+            // Then create the receiver and the listened methods
             .then(function (partitionIds) {
                 return partitionIds.map(function (partitionId) {
                     return client.createReceiver('$Default', partitionId, { 'startAfterTime' : Date.now()}).then(function(receiver) {
